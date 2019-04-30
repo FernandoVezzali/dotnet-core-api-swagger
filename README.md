31ab5de3ead2b158a6ba77358c64047e09c04bb6
Fernando <fernando@folhacerta.com.br>
2019-04-30 11:05:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotnet-core-api-sqlite
+Criando uma API com ASP.Net Core e SQLite
+
+[![Video](https://fernandovezzali.com/wp-content/uploads/2019/04/thumb-vid-01-retina.png)](https://youtu.be/9t_lBp0FmFs)
+
+## Post do projeto:
+https://fernandovezzali.com/criando-uma-api-com-asp-net-core-2-2-e-sqlite/
---
+++ app/Models/DemoDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace app.Models
+{
+    public partial class DemoDbContext : DbContext
+    {
+        public DemoDbContext()
+        {
+        }
+
+        public DemoDbContext(DbContextOptions<DemoDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=local.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
